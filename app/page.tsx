433d9176009b4c9db9e51385cc77cdeccbbe98d3
Zongjian <qingzhenghust@gmail.com>
2023-08-19 00:17:50 +0800

    update index

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ function NavLeftFunc() {
 
 export default function Nav() {
   return (
-    <div className="bg-white fixed flex w-screen">
+    <div className="bg-white flex w-screen">
       <div className="w-11/12 mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
         <div className="relative isolate overflow-hidden bg-gray-100 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
           <svg
